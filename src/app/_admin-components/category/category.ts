90772991bbe077c731a812d7db96cb8a78945171
omeruren <omer.uren19@gmail.com>
2025-10-03 09:30:24 +0300

    Alertify package added

--- src/app/_admin-components/category/category.ts
+++ src/app/_admin-components/category/category.ts
@@ -1,37 +1,36 @@
 import { Component } from '@angular/core';
 import { CategoryService } from '../../_services/category-service';
 import { CategoryDto } from '../../_models/category';
+declare const alertify: any;
 
 @Component({
   selector: 'admin-category',
   standalone: false,
   templateUrl: './category.html',
-  styleUrl: './category.css'
+  styleUrl: './category.css',
 })
 export class Category {
-/**
- *
- */
-constructor(private categoryService:CategoryService) {
-this.getCategories();
-}
-
-categories: CategoryDto[];
-newCategory : CategoryDto = new CategoryDto();
-
-getCategories(){
-  this.categoryService.getCategories().subscribe(
-    {
-      next: result => this.categories = result.data,
-      error: result => console.log(result)
-    }
-  )
-}
-  createCategory(){
-this.categoryService.create(this.newCategory).subscribe({
-  next: result => this.categories.push(result.data),
-  error: result => console.log(result)
-})
+  /**
+   *
+   */
+  constructor(private categoryService: CategoryService) {
+    this.getCategories();
   }
 
+  categories: CategoryDto[];
+  newCategory: CategoryDto = new CategoryDto();
+
+  getCategories() {
+    this.categoryService.getCategories().subscribe({
+      next: (result) => (this.categories = result.data),
+      error: (result) => console.log(result),
+    });
+  }
+  createCategory() {
+    this.categoryService.create(this.newCategory).subscribe({
+      next: (result) => this.categories.push(result.data),
+      error: (result) => alertify.error(result.error.errors[0].errorMessage),
+      complete: () => alertify.success('Category created successfully'),
+    });
+  }
 }
