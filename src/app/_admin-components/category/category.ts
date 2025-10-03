17354104acf0387e686d9d4a19a69a94fad40a0e
omeruren <omer.uren19@gmail.com>
2025-10-03 16:47:15 +0300

    Sweet Alert imported

--- src/app/_admin-components/category/category.ts
+++ src/app/_admin-components/category/category.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CategoryService } from '../../_services/category-service';
 import { CategoryDto } from '../../_models/category';
+import { Sweetalert } from '../../_services/sweetalert';
 declare const alertify: any;
 declare const bootstrap: any;
 
@@ -14,7 +15,7 @@ export class Category {
   /**
    *
    */
-  constructor(private categoryService: CategoryService) {
+  constructor(private categoryService: CategoryService, private swal: Sweetalert) {
     this.getCategories();
   }
 
@@ -28,33 +29,39 @@ export class Category {
     });
   }
   createCategory() {
-  this.categoryService.create(this.newCategory).subscribe({
-    next: (result) => {
-      this.categories.push(result.data);
-      alertify.success('Category created successfully');
-      // Close the  Modal
-      const modal = document.getElementById('createModal');
-      const modalInstance = bootstrap.Modal.getInstance(modal);
-      modalInstance?.hide();
-    },
-    error: (result) => {
-      alertify.error('An error occurred while creating the category.');
-      if (result.status === 400) {
-        this.errors = result.error.errors;
-      }
-    }
-  });
-}
-  delete(id){
-    this.categoryService.delete(id).subscribe({
-      error :result=> {
-        console.log(result.error);
-        alertify.error("An error occurred while deleting the category.")
+    this.categoryService.create(this.newCategory).subscribe({
+      next: (result) => {
+        this.categories.push(result.data);
+        alertify.success('Category created successfully');
+        // Close the  Modal
+        const modal = document.getElementById('createModal');
+        const modalInstance = bootstrap.Modal.getInstance(modal);
+        modalInstance?.hide();
       },
-      complete:()=>{
-        alertify.success("Category deleted successfully.");
-        this.getCategories();
-      }
+      error: (result) => {
+        alertify.error('An error occurred while creating the category.');
+        if (result.status === 400) {
+          this.errors = result.error.errors;
+        }
+      },
     });
+  }
+  async delete(id) {
+    const isConfirmed = await this.swal.areYouSure();
+
+    if (isConfirmed) {
+      this.categoryService.delete(id).subscribe({
+        error: (result) => {
+          console.log(result.error);
+          alertify.error('An error occurred while deleting the category.');
+        },
+        complete: () => {
+          alertify.success('Category deleted successfully.');
+          this.getCategories();
+        },
+      });
+    } else {
+      console.log('Operation cancelled.');
+    }
   }
 }
---
+++ src/app/_services/sweetalert.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import Swal, { SweetAlertResult } from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Sweetalert {
+  areYouSure(): Promise<boolean> {
+    return Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result: SweetAlertResult) => {
+      return result.isConfirmed;
+    });
+  }
+}
