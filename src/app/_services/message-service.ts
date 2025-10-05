44f39b2f803c3fc21b57407b50fca331e5958079
omeruren <omer.uren19@gmail.com>
2025-10-05 17:41:34 +0300

    MessageDto and  Service

---
+++ src/app/_services/message-service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Result } from '../_models/result';
+import { MessageDto } from '../_models/messageDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  baseUrl = 'https://localhost:7000/api/messages/';
+
+    /**
+     *
+     */
+    constructor(private http: HttpClient) {}
+
+    getAll() {
+      return this.http.get<Result<MessageDto[]>>(this.baseUrl);
+    }
+
+    getById(id: string) {
+      return this.http.get<Result<MessageDto>>(this.baseUrl + id);
+    }
+
+    create(messageDto: MessageDto) {
+      return this.http.post<Result<MessageDto>>(this.baseUrl, messageDto);
+    }
+
+    update(messageDto: MessageDto) {
+      return this.http.put(this.baseUrl, messageDto);
+    }
+    delete(id: string) {
+      return this.http.delete(this.baseUrl + id);
+    }
+}
