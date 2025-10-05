030fc09b80622328f51aa06bdd1f1ab2daf1de69
omeruren <omer.uren19@gmail.com>
2025-10-05 15:10:53 +0300

    Latest blogs in blog details page

--- src/app/_main-components/blog-details/blog-details.ts
+++ src/app/_main-components/blog-details/blog-details.ts
@@ -11,18 +11,27 @@ import { BlogDto } from '../../_models/blog';
 })
 export class BlogDetails {
   blog: BlogDto;
+  latestBlogs: BlogDto[];
 
   /**
    *
    */
   constructor(private blogService: BlogService, private route: ActivatedRoute) {
     this.getById();
+    this.getLatest5();
   }
 
   getById() {
     this.blogService.getById(this.route.snapshot.params['id']).subscribe({
       next: (result) => {
         this.blog = result.data;
+      },
+    });
+  }
+  getLatest5() {
+    this.blogService.getLatest5().subscribe({
+      next: (result) => {
+        this.latestBlogs = result.data;
       },
     });
   }
