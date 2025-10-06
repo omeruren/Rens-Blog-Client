47aaabf38010bb0312a28b089c6e7a9fcee5a317
omeruren <omer.uren19@gmail.com>
2025-10-06 15:11:48 +0300

    Contact Infos on main component

--- src/app/_main-components/contact-main/contact-main.ts
+++ src/app/_main-components/contact-main/contact-main.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactInfoService } from '../../_services/contact-info-service';
+import { ContactInfoDto } from '../../_models/contactInfoDto';
 
 @Component({
   selector: 'app-contact-main',
   standalone: false,
   templateUrl: './contact-main.html',
-  styleUrl: './contact-main.css'
+  styleUrl: './contact-main.css',
 })
-export class ContactMain {
+export class ContactMain implements OnInit{
+  /**
+   *
+   */
 
+  contactInfos: ContactInfoDto[];
+  constructor(private contactInfoService: ContactInfoService) {}
+
+
+  ngOnInit(): void {
+    this.getall();
+  }
+
+  getall() {
+    this.contactInfoService.getAll().subscribe({
+      next: (result) => {
+        this.contactInfos = result.data;
+      },
+      error: (result) => console.log(result.error),
+    });
+  }
 }
